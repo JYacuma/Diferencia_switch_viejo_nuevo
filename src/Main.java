9be3496d08ef3866597d9cbfbae1b59cdff463ed
JYacuma <jyacuma@ucundinamarca.edu.co>
2025-02-27 20:18:10 -0500

    Se agrego ejemplo switch viejo

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,41 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("En ramas se veran las diferencias entre el switch nuevo y switch viejo");
+        System.out.println("---DIAS DE LA SEMANA----");
+        System.out.println(" ");
+
+        int dia = 3;
+
+        String nombreDelDia;
+
+        switch (dia) {
+                case 1:
+                    nombreDelDia = "Lunes";
+                    break;
+                case 2:
+                    nombreDelDia = "Martes";
+                    break;
+                case 3:
+                    nombreDelDia = "Miércoles";
+                    break;
+                case 4:
+                    nombreDelDia = "Jueves";
+                    break;
+                case 5:
+                    nombreDelDia = "Viernes";
+                    break;
+                case 6:
+                    nombreDelDia = "Sábado";
+                    break;
+                case 7:
+                    nombreDelDia = "Domingo";
+                    break;
+                default:
+                    nombreDelDia = "Día inválido";
+                    break;
+            }
+
+            System.out.println("El día es: " + nombreDelDia);
 
 
     }
