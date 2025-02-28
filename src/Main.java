58c1a778146861f1d9cb6f0006adb52a1cdd459b
JYacuma <jyacuma@ucundinamarca.edu.co>
2025-02-27 20:05:26 -0500

    Se especifica que se va hacer

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("En ramas se veran las diferencias entre el switch nuevo y switch viejo");
+
+
+    }
+}
